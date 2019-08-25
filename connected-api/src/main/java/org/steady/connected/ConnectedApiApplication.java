077ac7f041aa1f6f8f0db6d2228d0d91bab3a61f
junsulime <vfv0121@gmail.com>
2019-08-25 20:56:57 +0900

    [#1] Initialize structure of project
    프로젝트의 패키지 및 모듈 구조를 잡았다. 자세한 설명은 위키에 적어놓도록
    하겠다.

--- connected-api/src/main/java/org/steady/connected/ConnectedApiApplication.java
+++ connected-api/src/main/java/org/steady/connected/ConnectedApiApplication.java
@@ -1,7 +1,11 @@
 package org.steady.connected;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConnectedApiApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(ConnectedApiApplication.class, args);
     }
 }
